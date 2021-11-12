544d2fb1853beb5114e4b742af4bfcac8148be23
Jason Michael <jason_mic2000@yahoo.com>
2021-11-12 13:46:29 +0530

    Added basic addition function

--- calculator.py
+++ calculator.py
@@ -12,9 +12,19 @@ def button_click(number):
     e.delete(0, END)
     e.insert(0, str(current) + str(number))
 
-def button_clear():
+def clear():
     e.delete(0, END)
 
+def addition():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def equal():
+    second_number = e.get()
+    e.delete(0, END)
+    e.insert(0, f_num + int(second_number))
 
 # Define buttons
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -27,9 +37,9 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_addition = Button(root, text="+", padx=39, pady=20, command=lambda: button_click())
-button_equal = Button(root, text="=", padx=88, pady=20, command=lambda: button_click())
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_addition = Button(root, text="+", padx=39, pady=20, command=addition)
+button_equal = Button(root, text="=", padx=88, pady=20, command=equal)
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=clear)
 
 # Put buttons on screen
 
