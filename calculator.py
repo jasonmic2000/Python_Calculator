34dfa74b1914d3ae61c9c6d0e4a3025ef62f12a0
Jason Michael <jason_mic2000@yahoo.com>
2021-11-12 12:38:31 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -21,7 +21,7 @@ button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
 button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
 button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
 button_addition = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=88, pady=20, command=button_add)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_add)
 
 # Put buttons on screen
@@ -41,8 +41,8 @@ button_9.grid(row=1, column=2)
 button_0.grid(row=4, column=0)
 
 button_addition.grid(row=5, column=0)
-button_clear.grid(row==4, column=1)
-button_equal.grid(row=5, column=1)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_equal.grid(row=5, column=1, columnspan=2)
 
 
 root.mainloop()
